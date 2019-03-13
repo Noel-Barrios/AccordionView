0fc1c085ce1395dbb6ec8f5bafc6c7e2cac2a67f
Noel Barrios <noel.barrios@optum.com>
2019-03-13 14:47:33 -0500

    create accordion view using CustomControls.AccordionView NuGet Package

---
+++ AccordionView/AccordionViewPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AccordionView
+{
+    public partial class AccordionViewPage : ContentPage
+    {
+        public AccordionViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_Clicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.nuget.org/packages/Xamarin.CustomControls.AccordionView"));
+        }
+    }
+}
